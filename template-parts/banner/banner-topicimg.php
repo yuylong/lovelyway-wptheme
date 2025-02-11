c4b4b851108b4b20c9d766a95e9eebd3f4c99e55
Yulong Yu <yuylong@acm.org>
2025-02-11 22:08:43 +0800

    Fix the error reading the topic image info.

--- template-parts/banner/banner-topicimg.php
+++ template-parts/banner/banner-topicimg.php
@@ -8,7 +8,7 @@
 <div class="post-container">
     <div class="post__wrap-post">
         <div>
-            <div class="post__wrap-sale" style="background-image: url('<?php echo $topic_image->url; ?>');">
+            <div class="post__wrap-sale" style="background-image: url('<?php echo esc_url( $topic_image['url'] ); ?>');">
                 <div class="post-content">
                     <?php
                     $content = substr(get_the_content(), 0, 250) . '...';
@@ -16,10 +16,10 @@
                     <div class="post-description">
                         <div class="desctiption__wrap">
                             <div class="description__wrap-title">
-                                <h2><?php echo $topic_image->caption; ?></h2>
+                                <h2><?php echo esc_html( $topic_image['caption'] ); ?></h2>
                             </div>
                         </div>
-                        <p><?php echo $topic_image->description; ?></p>
+                        <p><?php echo esc_html( $topic_image['description'] ); ?></p>
                     </div>
                 </div>
             </div>
